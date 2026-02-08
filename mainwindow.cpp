7f286195a9d55c60b4c4ca328680b8cbb47935f5
RADd2 <nikitaabramchik95@gmail.com>
2026-02-08 10:57:15 +0300

    Add all files

---
+++ mainwindow.cpp
@@ -0,0 +1,32 @@
+#include "mainwindow.h"
+
+#include <vector>
+#include <QPointF>
+#include <QVBoxLayout>
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , mode(new QComboBox(this))
+    , controller(new Controller)
+{
+    // ui->setupUi(this);
+
+    QWidget* control = new QWidget;
+    this->setCentralWidget(control);
+    QVBoxLayout* layout = new QVBoxLayout();
+    layout->addWidget(mode);
+    layout->addWidget(controller);
+    control->setLayout(layout);
+    repaint();
+
+    mode->addItem("Polygons");
+    mode->addItem("Light");
+
+    connect(mode, &QComboBox::activated, this, &MainWindow::ChangeMode);
+}
+
+MainWindow::~MainWindow()
+{
+    // delete ui;
+}
